04a7af0a0fc84032d803e6ada7c96042ecbd1eb6
Alexis Taberna <alexis.taberna@moorea.io>
2020-07-26 16:47:02 -0300

    fix get actual tag

--- scripts/git/get_actual_tag.sh
+++ scripts/git/get_actual_tag.sh
@@ -1,4 +1,4 @@
-LATEST_TAG=$(git describe --abbrev=0 --tags)
+LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 LATEST_TAG="${LATEST_TAG/'refs/heads/'/''}"
 LATEST_TAG="${LATEST_TAG/'v'/''}"
 
